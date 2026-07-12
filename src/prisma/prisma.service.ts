00ab781e0cd8159aadf33ebf8ca8f3b4c5c2be75
Pratik4230 <pratikjadhav1438@gmail.com>
2026-07-12 21:38:14 +0530

    Db setup

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from "@prisma/adapter-pg";
+
+
+@Injectable()
+export class PrismaService implements OnModuleInit {
+  private readonly client: PrismaClient;
+
+  constructor(){
+    const adapter = new PrismaPg(process.env.DATABASE_URL!);
+    this.client = new PrismaClient({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.client.$connect();
+  }
+
+  get user(){
+    return this.client.user;
+  }
+
+  get $transaction(){
+    return this.client.$transaction.bind(this.client)
+  }
+
+}
